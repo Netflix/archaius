2a34599bb423b035a9749db067fe7cd48b5c89f8
Allen Wang <awang@netflix.com>
2012-06-06 09:39:45 -0700

    Enable default configuration for DynamicProperty.getInstance().

--- src/main/java/com/netflix/config/DynamicProperty.java
+++ src/main/java/com/netflix/config/DynamicProperty.java
@@ -241,9 +241,9 @@ public class DynamicProperty {
     public static DynamicProperty getInstance(String propName) {
         // This is to ensure that a configuration source is registered with
         // DynamicProperty
-        /* if (dynamicPropertySupportImpl == null) {
+        if (dynamicPropertySupportImpl == null) {
             DynamicPropertyFactory.getInstance();
-        } */
+        }
         DynamicProperty prop = ALL_PROPS.get(propName);
         if (prop == null) {
             prop = new DynamicProperty(propName);
