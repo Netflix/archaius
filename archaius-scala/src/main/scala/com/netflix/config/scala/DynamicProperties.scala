469db5618bcad80a5c87369e384ec86992427f97
Greg Orzell <gorzell@sumologic.com>
2012-08-14 15:10:38 -0700

    Issue #15 and Issue #18 Added copyright notices.

--- archaius-scala/src/main/scala/com/netflix/config/scala/DynamicProperties.scala
+++ archaius-scala/src/main/scala/com/netflix/config/scala/DynamicProperties.scala
@@ -1,4 +1,20 @@
-import com.netflix.config.PropertyWrapper
+/*
+ *
+ *  Copyright 2012 Sumo Logic, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
 import com.netflix.config.scala._
 
 /**
@@ -61,8 +77,4 @@ trait DynamicProperties {
 
   protected def dynamicStringProperty(propertyName: String, default: String): DynamicStringProperty =
     dynamicStringProperty(propertyName, default, null)
-
-  private def addCallback(callback: Runnable, property: PropertyWrapper[_]) {
-    if (callback != null) property.addCallback(callback)
-  }
 }
