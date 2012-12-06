e88644f9d6c5fec5a59bb8511458386d98d24647
mhawthorne <mhawthorne@gmail.com>
2012-12-05 17:16:37 -0800

    adding copyrights

--- archaius-core/src/main/java/com/netflix/config/DerivedStringProperty.java
+++ archaius-core/src/main/java/com/netflix/config/DerivedStringProperty.java
@@ -1,3 +1,20 @@
+/*
+ *
+ *  Copyright 2012 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
 package com.netflix.config;
 
 import org.slf4j.Logger;
@@ -24,21 +41,6 @@ public abstract class DerivedStringProperty<D> extends DynamicStringProperty {
         deriveAndSet();
     }
 
-    private void deriveAndSet() {
-         try {
-            derived.set(derive(this.get()));
-        } catch (Exception e) {
-            log.error("error when deriving initial value", e);
-        }
-    }
-
-    @Override
-    protected void propertyChanged() {
-        super.propertyChanged();
-//        derived.set(this.derive(this.get()));
-        deriveAndSet();
-    }
-
     /**
      * Fetches derived value.
      */
@@ -50,5 +52,19 @@ public abstract class DerivedStringProperty<D> extends DynamicStringProperty {
      * Invoked when property is updated with a new value.  Should return the new derived value, which may be null.
      */
     protected abstract D derive(String value);
+
+    @Override
+    protected void propertyChanged() {
+        super.propertyChanged();
+        deriveAndSet();
+    }
+
+    private void deriveAndSet() {
+         try {
+            derived.set(derive(this.get()));
+        } catch (Exception e) {
+            log.error("error when deriving initial value", e);
+        }
+    }
 
 }
