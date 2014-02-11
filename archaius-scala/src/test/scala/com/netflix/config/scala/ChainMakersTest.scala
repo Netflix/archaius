4b326b5690347255f1fd1221c68d3c89f0ff1f70
Stu (Matt Cary) <mcary@blackpearlsystems.com>
2014-02-11 14:19:41 -0800

    Add tests for ChainMakers.deriveChain()

--- archaius-scala/src/test/scala/com/netflix/config/scala/ChainMakersTest.scala
+++ archaius-scala/src/test/scala/com/netflix/config/scala/ChainMakersTest.scala
@@ -15,15 +15,19 @@
  */
 package com.netflix.config.scala
 
+import com.netflix.config.{ChainedDynamicProperty, DynamicIntProperty => JavaDynamicIntProperty, DynamicPropertyFactory}
 import org.junit.runner.RunWith
 import org.scalatest.WordSpec
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.matchers.ShouldMatchers
 
 @RunWith(classOf[JUnitRunner])
-class ChainMakersTest extends WordSpec with ShouldMatchers {
+class ChainMakersTest extends ChainedPropertiesTest with ShouldMatchers {
 
   import ChainMakers._
+
+  private def wrapRoot(p: String, r: JavaDynamicIntProperty) = new ChainedDynamicProperty.IntProperty(p, r)
+  private def wrapLink(p: String, n: ChainedDynamicProperty.IntProperty) = new ChainedDynamicProperty.IntProperty(p, n)
 
   "fanPropertyName" should {
     "understand a single-part name with prefix and suffix" in {
@@ -31,6 +35,78 @@ class ChainMakersTest extends WordSpec with ShouldMatchers {
     }
     "understand a dual-part name with prefix and suffix" in {
       fanPropertyName(Some("x"), "a.b", Some("y")) should equal(Seq("x.y", "x.a.y", "x.a.b.y"))
+    }
+  }
+
+  "deriveChain" should {
+    "produce a chain given property names and helper functions" in {
+      val baseName = "foo"
+      val topName = s"${baseName}.bar"
+      val default = -1
+      Seq(baseName, topName) foreach clearProperty
+      val chain = deriveChain( Seq(topName), DynamicPropertyFactory.getInstance.getIntProperty(baseName, default), wrapRoot, wrapLink )
+      chain should not be null
+      chain.getName should be(topName)
+      chain.getDefaultValue should be(default)
+    }
+    "produce a chain which produces the default value when properties are not set" in {
+      val baseName = "foo"
+      val topName = s"${baseName}.bar"
+      val default = -1
+      Seq(baseName, topName) foreach clearProperty
+      val chain = deriveChain( Seq(topName), DynamicPropertyFactory.getInstance.getIntProperty(baseName, default), wrapRoot, wrapLink )
+      chain should not be null
+      chain.getName should be(topName)
+      chain.get() should be(default)
+    }
+    "produce a chain which responds to setting the root property" in {
+      val baseName = "foo"
+      val topName = s"${baseName}.bar"
+      val baseValue = 1
+      val default = -1
+      Seq(baseName, topName) foreach clearProperty
+      val chain = deriveChain( Seq(topName), DynamicPropertyFactory.getInstance.getIntProperty(baseName, default), wrapRoot, wrapLink )
+      chain should not be null
+      chain.get() should be(default)
+      setProperty(baseName, baseValue)
+      chain.get() should be(baseValue)
+    }
+    "produce a chain which responds to setting a non-root property" in {
+      val baseName = "foo"
+      val topName = s"${baseName}.bar"
+      val topValue = 2
+      val default = -1
+      Seq(baseName, topName) foreach clearProperty
+      val chain = deriveChain( Seq(topName), DynamicPropertyFactory.getInstance.getIntProperty(baseName, default), wrapRoot, wrapLink )
+      chain should not be null
+      chain.get() should be(default)
+      setProperty(topName, topValue)
+      chain.get() should be(topValue)
+    }
+    "produce a chain which prefers a non-root property's value over the root property" in {
+      val baseName = "foo"
+      val topName = s"${baseName}.bar"
+      val baseValue = 1
+      val topValue = 2
+      val default = -1
+      Seq(baseName, topName) foreach clearProperty
+      val chain = deriveChain( Seq(topName), DynamicPropertyFactory.getInstance.getIntProperty(baseName, default), wrapRoot, wrapLink )
+      chain should not be null
+      chain.get() should be(default)
+      setProperty(baseName, baseValue)
+      chain.get() should be(baseValue)
+      setProperty(topName, topValue)
+      chain.get() should be(topValue)
+    }
+    "explode given null property names" in {
+      intercept[NullPointerException] {
+        deriveChain( null, DynamicPropertyFactory.getInstance.getIntProperty("foo", -1), wrapRoot, wrapLink )
+      }
+    }
+    "explode given no property names" in {
+      intercept[NoSuchElementException] {
+        deriveChain( Seq.empty, DynamicPropertyFactory.getInstance.getIntProperty("foo", -1), wrapRoot, wrapLink )
+      }
     }
   }
 }
