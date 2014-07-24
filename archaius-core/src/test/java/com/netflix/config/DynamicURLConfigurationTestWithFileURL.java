d664ac7a7f61a1a4327286de40200b7a5b66cc20
vgovindaswamy <vgovindaswamy@ziji-media.com>
2014-07-24 17:10:57 -0600

    incorporated PR comment to reduce the sleep time and changed to DynamicURLConfiguration config = new DynamicURLConfiguration(0, 500, false, url)

--- archaius-core/src/test/java/com/netflix/config/DynamicURLConfigurationTestWithFileURL.java
+++ archaius-core/src/test/java/com/netflix/config/DynamicURLConfigurationTestWithFileURL.java
@@ -24,12 +24,13 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.apache.commons.configuration.AbstractConfiguration;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class DynamicURLConfigurationTestWithFileURL {
-    
+
     @BeforeClass
     public static void init() {
         System.setProperty("archaius.configurationSource.defaultFileName", "test.properties");
@@ -40,39 +41,37 @@ public class DynamicURLConfigurationTestWithFileURL {
         DynamicURLConfiguration config = new DynamicURLConfiguration();
         Assert.assertEquals(5, config.getInt("com.netflix.config.samples.SampleApp.SampleBean.numSeeds"));
     }
-    
 
+    
     @Test
     public void testFileURLWithPropertiesUpdatedDynamically() throws IOException, InterruptedException {
 
         File file = new File("src/test/resources/test.properties");
-        
-        populateFile(file, "test.host=12312,123213", "test.host1=132,12");
-        Thread.sleep(1 * 31000);
-        DynamicURLConfiguration config = new DynamicURLConfiguration();
+        populateFile(file, "test.host=12312,123213", "test.host1=13212");
 
-        Assert.assertEquals(5, config.getInt("com.netflix.config.samples.SampleApp.SampleBean.numSeeds"));
-        Thread.sleep(1 * 31000);
+        AbstractConfiguration.setDefaultListDelimiter(',');
+        DynamicURLConfiguration config = new DynamicURLConfiguration(0, 500, false, file.toURI().toString());
+        Thread.sleep(1000);
+        Assert.assertEquals(13212, config.getInt("test.host1"));
+        Thread.sleep(1000);
+        populateFile(file, "test.host=12312,123213", "test.host1=13212");
+        populateFile(file, "test.host=12312,123213", "test.host1=13212");
         CopyOnWriteArrayList writeList = new CopyOnWriteArrayList();
         writeList.add("12312");
         writeList.add("123213");
-
+        config.setProperty("sample.domain", "google,yahoo");
         Assert.assertEquals(writeList, config.getProperty("test.host"));
+        
+
     }
 
-    /**
-     * @param temporary
-     * @param prop3
-     * @param prop4
-     * @throws IOException
-     */
     private void populateFile(File temporary, String prop1, String prop2) throws IOException {
 
         String s = prop1 + "\n" + prop2 + "\n";
         byte data[] = s.getBytes("UTF-8");
         OutputStream out = null;
         try {
-            out = new BufferedOutputStream(new FileOutputStream(temporary,true), 8 * 1024);
+            out = new BufferedOutputStream(new FileOutputStream(temporary, true), 8 * 1024);
             out.write(data, 0, data.length);
         } finally {
             if (null != out) {
@@ -82,4 +81,5 @@ public class DynamicURLConfigurationTestWithFileURL {
         }
 
     }
+
 }
