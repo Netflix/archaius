01c1de4a9271c6fb9ba4b2266fe6502341162e2a
elandau <elandau@yahoo.com>
2015-03-05 23:29:14 -0800

    Improved parsing.  Parse lists.

--- archaius-core/src/main/java/netflix/archaius/config/AbstractConfig.java
+++ archaius-core/src/main/java/netflix/archaius/config/AbstractConfig.java
@@ -12,6 +12,8 @@ import netflix.archaius.Config;
 import netflix.archaius.StrInterpolator;
 import netflix.archaius.interpolate.CommonsStrInterpolatorFactory;
 
+import com.google.common.collect.Lists;
+
 public abstract class AbstractConfig implements Config {
 
     private final String name;
@@ -57,8 +59,13 @@ public abstract class AbstractConfig implements Config {
     public Long getLong(String key) {
         String value = getString(key);
         if (value == null) 
+            return notFound(null);
+        try {
+            return Long.parseLong(value);
+        }
+        catch (NumberFormatException e) {
             return null;
-        return Long.valueOf(value);
+        }
     }
 
     @Override
@@ -66,14 +73,19 @@ public abstract class AbstractConfig implements Config {
         String value = getString(key);
         if (value == null) 
             return defaultValue;
-        return Long.valueOf(value);
+        try {
+            return Long.parseLong(value);
+        }
+        catch (NumberFormatException e) {
+            return defaultValue;
+        }
     }
 
     @Override
     public String getString(String key) {
         Object value = getRawProperty(key);
         if (value == null) 
-            return null;
+            return notFound();
         return value.toString();
     }
 
@@ -81,7 +93,7 @@ public abstract class AbstractConfig implements Config {
     public String getString(String key, String defaultValue) {
         String value = getString(key);
         if (value == null) 
-            return defaultValue;
+            return notFound(defaultValue);
         return value;
     }
 
@@ -89,40 +101,69 @@ public abstract class AbstractConfig implements Config {
     public Double getDouble(String key) {
         String value = getString(key);
         if (value == null) 
-            return null;
-        return Double.valueOf(value);
+            return notFound();
+        try {
+            return Double.parseDouble(value);
+        }
+        catch (NumberFormatException e) {
+            return parseError(e);
+        }
     }
 
     @Override
     public Double getDouble(String key, Double defaultValue) {
         String value = getString(key);
         if (value == null) 
-            return defaultValue;
-        return Double.valueOf(value);
+            return notFound(defaultValue);
+        try {   
+            return Double.parseDouble(value);
+        }
+        catch (NumberFormatException e) {
+            return parseError(e, defaultValue);
+        }
     }
 
     @Override
     public Integer getInteger(String key) {
         String value = getString(key);
         if (value == null) 
-            return null;
-        return Integer.valueOf(value);
+            return notFound();
+
+        try {   
+            return Integer.parseInt(value);
+        }
+        catch (NumberFormatException e) {
+            return parseError(e);
+        }
     }
 
     @Override
     public Integer getInteger(String key, Integer defaultValue) {
         String value = getString(key);
         if (value == null) 
-            return defaultValue;
-        return Integer.valueOf(value);
+            return notFound(defaultValue);
+        
+        try {
+            return Integer.parseInt(value);
+        }
+        catch (NumberFormatException e) {
+            return parseError(e, defaultValue);
+        }
     }
 
     @Override
     public Boolean getBoolean(String key) {
         String value = getString(key);
         if (value == null) 
-            return null;
-        return Boolean.valueOf(value);
+            return notFound(null);
+        
+        if (value.equalsIgnoreCase("true") || value.equalsIgnoreCase("yes") || value.equalsIgnoreCase("on")) {
+            return Boolean.TRUE;
+        } 
+        else if (value.equalsIgnoreCase("false") || value.equalsIgnoreCase("no") || value.equalsIgnoreCase("off")) {
+            return Boolean.FALSE;
+        }
+        return parseError(null);
     }
 
     @Override
@@ -130,99 +171,164 @@ public abstract class AbstractConfig implements Config {
         String value = getString(key);
         if (value == null) 
             return defaultValue;
-        return Boolean.valueOf(value);
+        if (value.equalsIgnoreCase("true") || value.equalsIgnoreCase("yes") || value.equalsIgnoreCase("on")) {
+            return Boolean.TRUE;
+        } 
+        else if (value.equalsIgnoreCase("false") || value.equalsIgnoreCase("no") || value.equalsIgnoreCase("off")) {
+            return Boolean.FALSE;
+        }
+        return parseError(null, defaultValue);
     }
 
     @Override
     public Short getShort(String key) {
         String value = getString(key);
         if (value == null) 
-            return null;
-        return Short.valueOf(value);
+            return notFound();
+        
+        try {
+            return Short.parseShort(value);
+        }
+        catch (NumberFormatException e) {
+            return parseError(e);
+        }
     }
 
     @Override
     public Short getShort(String key, Short defaultValue) {
         String value = getString(key);
         if (value == null) 
-            return defaultValue;
-        return Short.valueOf(value);
+            return notFound(defaultValue);
+        try {
+            return Short.parseShort(value);
+        }
+        catch (NumberFormatException e) {
+            return parseError(e, defaultValue);
+        }
     }
 
     @Override
     public BigInteger getBigInteger(String key) {
         String value = getString(key);
         if (value == null) 
-            return null;
-        return BigInteger.valueOf(Long.valueOf(value));
+            return notFound();
+        try {
+            return BigInteger.valueOf(Long.valueOf(value));
+        }
+        catch (NumberFormatException e) {
+            return parseError(e);
+        }
     }
 
     @Override
     public BigInteger getBigInteger(String key, BigInteger defaultValue) {
         String value = getString(key);
         if (value == null) 
-            return defaultValue;
-        return BigInteger.valueOf(Long.valueOf(value));
+            return notFound();
+        try {
+            return BigInteger.valueOf(Long.valueOf(value));
+        }
+        catch (NumberFormatException e) {
+            return parseError(e, defaultValue);
+        }
     }
 
     @Override
     public BigDecimal getBigDecimal(String key) {
         String value = getString(key);
         if (value == null) 
-            return null;
-        return BigDecimal.valueOf(Long.valueOf(value));
+            return notFound();
+        try {
+            return BigDecimal.valueOf(Long.valueOf(value));
+        }
+        catch (NumberFormatException e) {
+            return parseError(e);
+        }
     }
 
     @Override
     public BigDecimal getBigDecimal(String key, BigDecimal defaultValue) {
         String value = getString(key);
         if (value == null) 
-            return defaultValue;
-        return BigDecimal.valueOf(Long.valueOf(value));
+            return notFound(defaultValue);
+        try {
+            return BigDecimal.valueOf(Long.valueOf(value));
+        }
+        catch (NumberFormatException e) {
+            return parseError(e, defaultValue);
+        }
     }
 
     @Override
     public Float getFloat(String key) {
         String value = getString(key);
         if (value == null) 
-            return null;
-        return Float.valueOf(value);
+            return notFound();
+        try {
+            return Float.parseFloat(value);
+        }
+        catch (NumberFormatException e) {
+            return parseError(e);
+        }
     }
 
     @Override
     public Float getFloat(String key, Float defaultValue) {
         String value = getString(key);
         if (value == null) 
-            return defaultValue;
-        return Float.valueOf(value);
+            return notFound();
+        try {
+            return Float.parseFloat(value);
+        }
+        catch (NumberFormatException e) {
+            return parseError(e);
+        }
     }
 
     @Override
     public Byte getByte(String key) {
         String value = getString(key);
         if (value == null) 
-            return null;
-        return Byte.valueOf(value);
+            return notFound();
+        try {
+            return Byte.parseByte(value);
+        }
+        catch (NumberFormatException e) {
+            return parseError(e);
+        }
     }
 
     @Override
     public Byte getByte(String key, Byte defaultValue) {
         String value = getString(key);
         if (value == null) 
-            return defaultValue;
-        return Byte.valueOf(value);
+            return notFound(defaultValue);
+        try {
+            return Byte.parseByte(value);
+        }
+        catch (NumberFormatException e) {
+            return parseError(e, defaultValue);
+        }
     }
 
     @Override
     public List getList(String key) {
-        // TODO Auto-generated method stub
-        return null;
+        String value = getString(key);
+        if (value == null) {
+            return notFound();
+        }
+        String[] parts = value.split(",");
+        return Lists.newArrayList(parts);
     }
 
     @Override
     public List getList(String key, List defaultValue) {
-        // TODO Auto-generated method stub
-        return null;
+        String value = getString(key);
+        if (value == null) {
+            return notFound(defaultValue);
+        }
+        String[] parts = value.split(",");
+        return Lists.newArrayList(parts);
     }
 
     @Override
@@ -267,6 +373,22 @@ public abstract class AbstractConfig implements Config {
         return result.iterator();
     }
 
+    private <T> T notFound(T defaultValue) {
+        return defaultValue;
+    }
+    
+    private <T> T notFound() {
+        return null;
+    }
+    
+    private <T> T parseError(Exception e, T defaultValue) {
+        return defaultValue;
+    }
+    
+    private <T> T parseError(Exception e) {
+        return null;
+    }
+    
     @Override
     public Config subset(String prefix) {
         return new PrefixedViewConfig(prefix, this);
