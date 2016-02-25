4c483f60dd6ce129c2ca60a9eb8143f9fc0f2bfc
elandau <elandau@netflix.com>
2016-02-25 15:52:49 -0800

    Fix tests

--- archaius-core/src/test/java/com/netflix/config/DynamicMapPropertyTest.java
+++ archaius-core/src/test/java/com/netflix/config/DynamicMapPropertyTest.java
@@ -1,38 +1,52 @@
 package com.netflix.config;
 
 import java.util.Arrays;
-import java.util.Set;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
 
 public class DynamicMapPropertyTest {
     
-    public class DynamicIntegerSetProperty extends DynamicSetProperty<Integer> {
-        public DynamicIntegerSetProperty(String propName, Set<Integer> defaultValue) {
+    public class DynamicIntegerMapProperty extends DynamicMapProperty<Integer, String> {
+        public DynamicIntegerMapProperty(String propName, Map<Integer, String> defaultValue) {
             super(propName, defaultValue);
         }
 
         @Override
-        protected Integer from(String value) {
+        protected String getValue(String key) {
+            return key;
+        }
+
+        @Override
+        protected Integer getKey(String value) {
             return Integer.parseInt(value);
         }
     }
     
 
     @Test
-    public void testCallbacksAddUnsubscribe() {
-        DynamicIntegerSetProperty dp = new DynamicIntegerSetProperty("testProperty", Sets.newTreeSet(Arrays.asList(1,2,3,4,5)));
-        Set<Integer> current = dp.get();
+    public void test() throws InterruptedException {
+        Map<Integer, String> map1 = new LinkedHashMap<Integer, String>();
+        map1.put(5, "d");
+        map1.put(1, "a");
+        map1.put(2, "b");
+        map1.put(3, "c");
+        map1.put(4, "c");
         
-        Assert.assertEquals(Arrays.asList(1,2,3,4,5), Lists.newArrayList(current));
+        DynamicIntegerMapProperty dp = new DynamicIntegerMapProperty("testProperty", map1);
         
-        ConfigurationManager.getConfigInstance().setProperty("testProperty", "1,2,3,4,5,6,7,8,9");
-        current = dp.get();
+        Map<Integer, String> current = dp.getMap();
         
-        Assert.assertEquals(Arrays.asList(1,2,3,4,5,6,7,8,9), Lists.newArrayList(current));
+        Assert.assertEquals(Arrays.asList(5,1,2,3,4), Lists.newArrayList(current.keySet()));
+        
+        ConfigurationManager.getConfigInstance().setProperty("testProperty", "1=a,2=a,3=a,4=a,5=a,6=a,7=a,8=a,9=a");
+        current = dp.getMap();
+        
+        Assert.assertEquals(Arrays.asList(1,2,3,4,5,6,7,8,9), Lists.newArrayList(current.keySet()));
     }
 }
--- archaius-core/src/test/java/com/netflix/config/DynamicSetPropertyTest.java
+++ archaius-core/src/test/java/com/netflix/config/DynamicSetPropertyTest.java
@@ -1,50 +1,38 @@
 package com.netflix.config;
 
 import java.util.Arrays;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.Set;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 
 public class DynamicSetPropertyTest {
     
-    public class DynamicIntegerMapProperty extends DynamicMapProperty<Integer, String> {
-        public DynamicIntegerMapProperty(String propName, Map<Integer, String> defaultValue) {
+    public class DynamicIntegerSetProperty extends DynamicSetProperty<Integer> {
+        public DynamicIntegerSetProperty(String propName, Set<Integer> defaultValue) {
             super(propName, defaultValue);
         }
 
         @Override
-        protected String getValue(String key) {
-            return key;
-        }
-
-        @Override
-        protected Integer getKey(String value) {
+        protected Integer from(String value) {
             return Integer.parseInt(value);
         }
     }
     
 
     @Test
-    public void testCallbacksAddUnsubscribe() {
-        Map<Integer, String> map1 = new TreeMap<Integer, String>();
-        map1.put(1, "a");
-        map1.put(2, "b");
-        map1.put(3, "c");
-        map1.put(4, "d");
+    public void test() {
+        DynamicIntegerSetProperty dp = new DynamicIntegerSetProperty("testProperty", Sets.newLinkedHashSet(Arrays.asList(5,1,2,3,4)));
+        Set<Integer> current = dp.get();
         
-        DynamicIntegerMapProperty dp = new DynamicIntegerMapProperty("testProperty", map1);
+        Assert.assertEquals(Arrays.asList(5,1,2,3,4), Lists.newArrayList(current));
         
-        Map<Integer, String> current = dp.getMap();
+        ConfigurationManager.getConfigInstance().setProperty("testProperty", "2,3,4,5,6,7,8,9,1");
+        current = dp.get();
         
-        Assert.assertEquals(Arrays.asList(1,2,3,4,5), Lists.newArrayList(map1.keySet()));
-        
-        ConfigurationManager.getConfigInstance().setProperty("testProperty", "1=a,2=a,3=a,4=a,5=a,6=a,7=a,8=a,9=a");
-        current = dp.getMap();
-        
-        Assert.assertEquals(Arrays.asList(1,2,3,4,5,6,7,8,9), Lists.newArrayList(map1.keySet()));
+        Assert.assertEquals(Arrays.asList(2,3,4,5,6,7,8,9,1), Lists.newArrayList(current));
     }
 }
