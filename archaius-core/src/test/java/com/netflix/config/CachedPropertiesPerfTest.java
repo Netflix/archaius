d3eefc7a368c0b62331fbab2703cd76b1a973497
Michael Smith <michaels@netflix.com>
2016-08-18 17:50:53 -0700

    Add some more info the CachedPropertiesPerfTest output

--- archaius-core/src/test/java/com/netflix/config/CachedPropertiesPerfTest.java
+++ archaius-core/src/test/java/com/netflix/config/CachedPropertiesPerfTest.java
@@ -29,15 +29,17 @@ public class CachedPropertiesPerfTest
         DynamicBooleanProperty newProp = new CachedDynamicBooleanProperty("zuul.test.cachedprops.new", true);
         runDynamicBooleanPropertyTest(newProp, 10000);
         long durationPrimitive = runDynamicBooleanPropertyTest(newProp, loopCount);
+        double avgPrimitiveNs = (durationPrimitive * 1000d * 1000d) / loopCount;
 
         // Warmup and then run benchmark.
         DynamicBooleanProperty oldProp = new DynamicBooleanProperty("zuul.test.cachedprops.original", true);
         runDynamicBooleanPropertyTest(oldProp, 10000);
         long durationOriginal = runDynamicBooleanPropertyTest(oldProp, loopCount);
+        double avgOriginalNs = (durationOriginal * 1000d * 1000d) / loopCount;
 
         System.out.println("#####################");
-        System.out.println("DynamicBooleanProperty totalled " + durationOriginal + " ms.");
-        System.out.println("CachedDynamicBooleanProperty totalled " + durationPrimitive + " ms.");
+        System.out.println("DynamicBooleanProperty: " + loopCount + " calls. Total = " + durationOriginal + " ms. Avg = " + avgOriginalNs + " nanos.");
+        System.out.println("CachedDynamicBooleanProperty: " + loopCount + " calls. Total = " + durationPrimitive + " ms. Avg = " + avgPrimitiveNs + " nanos.");
     }
 
     private static long runDynamicBooleanPropertyTest(DynamicBooleanProperty prop, long loopCount)
