49c28f464d16a286f4adfdcd5c8d3f65a78a21c6
Eran Landau <elandau@netflix.com>
2017-09-21 17:53:08 -0700

    More meaningful test names

--- archaius2-archaius1-bridge/src/test/java/com/netflix/archaius/bridge/StaticBridgeAddConfigurationTest.java
+++ archaius2-archaius1-bridge/src/test/java/com/netflix/archaius/bridge/StaticBridgeAddConfigurationTest.java
@@ -1,7 +1,5 @@
 package com.netflix.archaius.bridge;
 
-import javax.inject.Inject;
-
 import org.apache.commons.configuration.event.ConfigurationEvent;
 import org.apache.commons.configuration.event.ConfigurationListener;
 import org.junit.Test;
@@ -14,11 +12,13 @@ import com.netflix.archaius.api.inject.RuntimeLayer;
 import com.netflix.archaius.guice.ArchaiusModule;
 import com.netflix.config.ConfigurationManager;
 
+import javax.inject.Inject;
+
 public class StaticBridgeAddConfigurationTest {
 	private static ConfigurationListener listener = Mockito.mock(ConfigurationListener.class);
 	
 	public static class Foo {
-		public static void doSomethingStaticy() {
+		public static void addListenerBeforeBridgeInitialization() {
 			ConfigurationManager.getConfigInstance().addConfigurationListener(listener);
 		}
 	}
@@ -28,19 +28,21 @@ public class StaticBridgeAddConfigurationTest {
 	SettableConfig settableConfig;
 	
 	@Test
-	public void test() {
+	public void listenerAddedToStaticBeforeStaticInjection() {
 		Guice.createInjector(new ArchaiusModule(), new StaticArchaiusBridgeModule(), new AbstractModule() {
 			@Override
 			protected void configure() {
-				Foo.doSomethingStaticy();
+				Foo.addListenerBeforeBridgeInitialization();
 				this.requestInjection(StaticBridgeAddConfigurationTest.this);
 			}
 		});
-		
+
+		// Verify that the listener is called
 		Mockito.verify(listener, Mockito.never()).configurationChanged(Mockito.isA(ConfigurationEvent.class));
 		settableConfig.setProperty("foo", "bar");
 		Mockito.verify(listener, Mockito.times(2)).configurationChanged(Mockito.isA(ConfigurationEvent.class));
 		
+		// Listener no longer invoked after reset
 		StaticArchaiusBridgeModule.resetStaticBridges();
 		settableConfig.setProperty("bar", "baz");
 		Mockito.verify(listener, Mockito.times(2)).configurationChanged(Mockito.isA(ConfigurationEvent.class));
