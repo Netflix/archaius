ccf2396a845646e40b006c5408c252d520481b47
Milan Baran <milan.baran@nike.sk>
2017-12-18 17:25:07 +0100

    Test fix - It is safe to assume that some config reader will be provided from public module.

--- archaius2-guice/src/test/java/com/netflix/archaius/guice/InternalArchaiusModuleTest.java
+++ archaius2-guice/src/test/java/com/netflix/archaius/guice/InternalArchaiusModuleTest.java
@@ -1,6 +1,10 @@
 package com.netflix.archaius.guice;
 
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
+import com.google.inject.multibindings.Multibinder;
+import com.netflix.archaius.api.ConfigReader;
+import com.netflix.archaius.readers.PropertiesConfigReader;
 import org.junit.Test;
 
 public class InternalArchaiusModuleTest {
@@ -8,7 +12,14 @@ public class InternalArchaiusModuleTest {
     @Test
     public void succeedOnDuplicateInstall() {
         Guice.createInjector(
-                new InternalArchaiusModule(),
-                new InternalArchaiusModule());
+            new AbstractModule() {
+                @Override
+                protected void configure() {
+                    Multibinder.newSetBinder(this.binder(), ConfigReader.class).addBinding().to(PropertiesConfigReader.class).asEagerSingleton();
+                }
+            },
+            new InternalArchaiusModule(),
+            new InternalArchaiusModule()
+        );
     }
 }
